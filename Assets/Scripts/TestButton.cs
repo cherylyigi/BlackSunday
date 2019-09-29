69094d51cce8233730787e2fbcae410849ff1661
Tony Xie <xie.tony@yahoo.com>
2019-09-28 22:56:46 -0400

    Added popup message Canvas

---
+++ Assets/Scripts/TestButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    public GameObject Canvas;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            DisplayCanvas("asd");
+        }
+    }
+
+    public void DisplayCanvas(string text) {
+        if (Canvas != null) {
+            Canvas.GetComponent<UpdateCanvas>().DisplayCanvas(text);
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCanvas : MonoBehaviour
+{    
+    public GameObject Canvas;
+
+    public void CloseCanvas() {
+        if (Canvas != null) {
+            Canvas.SetActive(false);
+        }
+    }
+
+    public void DisplayCanvas(string text) {
+        if (Canvas != null) {
+            Text message = Canvas.transform.GetChild(1).gameObject.GetComponent<Text>();
+            message.text = text;
+            Canvas.SetActive(true);
+        }
+    }  
+}
