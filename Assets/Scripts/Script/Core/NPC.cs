ea77a1c6d8d944e668918401d92855f90ffd4994
cherylyigi <31687227+cherylyigi@users.noreply.github.com>
2019-09-29 02:09:51 -0400

    add Dialogue

---
+++ Assets/Scripts/Script/Core/NPC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class NPC : MonoBehaviour {
+
+    public Transform ChatBackGround;
+    public Transform Player;
+
+    public UpdateCanvas canvas;
+    private bool isOpen = false;
+
+    [TextArea(5, 10)]
+    public string[] sentences;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!isOpen)
+        {
+            canvas.DisplayCanvas(sentences[0]);
+            isOpen = true;
+        }
+    }
+
+    public void OnTriggerExit()
+    {
+    }
+}
+
